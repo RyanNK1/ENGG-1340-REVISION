3a34a721dabbad2b058a8b61919b580bf7733986
ryan <ryan.kimani0@gmail.com>
2025-04-01 16:56:21 +0800

    First Demo

---
+++ module8/Demos.cpp
@@ -0,0 +1,18 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int x = 10, y;
+    int * ptr = &x;
+    y = *ptr;
+    *ptr = 20;
+    cout << x << ' ' << y << endl;
+    cout << *ptr << endl;
+    ptr = &y;
+    (*ptr)++;
+    cout << x << ' ' << y << endl;
+    cout << *ptr << endl;
+
+    return 0;
+}
